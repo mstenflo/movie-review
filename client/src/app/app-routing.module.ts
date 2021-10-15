62e565fa745e2b741d3242f747042d3b4347e43b
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-15 15:24:46 -0500

    Update app-routing.module.ts
    Updated: app-routing.module.ts.
    -> Implemented routing for the main page, the login page, and the register page.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: MainPageComponent},
+  {path: "login", component: LoginPageComponent},
+  {path: "register", component: RegisterPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
