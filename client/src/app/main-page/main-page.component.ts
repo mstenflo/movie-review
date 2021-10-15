51d201e4a51dd64fe4470134274e6d591ece372f
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-15 17:03:47 -0500

    Update main-page.component.ts
    Updated: main-page-component.ts.
    -> Implemented functionality for retrieving a collection of Movies.

--- client/src/app/main-page/main-page.component.ts
+++ client/src/app/main-page/main-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../movie.service';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'app-main-page',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor() { }
+  movies : Movie[] = [];
+
+  constructor(private movieService : MovieService) {
+    console.log(this.movies);
+   }
 
   ngOnInit(): void {
+    this.getMovies();
   }
+
+  
+  public getMovies() : void {
+    this.movieService.getMovies().subscribe(response => (this.movies = response.items));
+  }
+  
 
 }
