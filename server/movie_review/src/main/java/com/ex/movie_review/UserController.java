96bddb18b6c781eb3421aaa40d78914b7088f1ad
mstenflo <stenflo@gmail.com>
2021-10-15 17:21:49 -0700

    create user controller

--- server/movie_review/src/main/java/com/ex/movie_review/UserController.java
+++ server/movie_review/src/main/java/com/ex/movie_review/UserController.java
@@ -3,9 +3,11 @@ package com.ex.movie_review;
 import models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping(path = "users")
@@ -25,5 +27,25 @@ public class UserController {
         user.setPassword("123");
         user.setPhone("123-123-1231");
         return user;
+    }
+
+//    @GetMapping(path="u")
+//    public User GetUserWithId(@RequestParam(value = "id", required = false) Integer userId) {
+//        System.out.println("Getting user with id " + userId);
+//        return new User();
+//    }
+
+    @GetMapping(path="{id}")
+    public User getUserById(@PathVariable Integer id) {
+        System.out.println("Getting user with id from path " + id);
+        User user = new User();
+        user.setId(id);
+        return user;
+    }
+
+    @PostMapping()
+    public ResponseEntity createNewUser(@RequestBody User user) throws URISyntaxException {
+        System.out.println("Creating new user");
+        return ResponseEntity.created(new URI("http://localhost:8001/api/users/4")).build();
     }
 }
