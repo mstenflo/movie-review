d6c726492c75d9d75104d9eee35dbd509dc6f763
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-17 14:44:54 -0500

    Update app.module.ts
    Updated: app.module.ts
    -> Imported the MatProgressSpinnerModule, which will allow us to use MaterialUI spinners for when something is being loaded.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -22,7 +24,8 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
