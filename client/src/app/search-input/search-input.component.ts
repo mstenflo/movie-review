d43e04010141ebb5a9fc40855117b6672863b867
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-18 21:58:18 -0500

    Create SearchInput Component
    Created: SearchInput Angular Component.
    -> This component will be responsible for taking in user input, and searching for movies that fit the user input.
    -> The list of searched movies might be refactored out at a later date, as it violates single responsibility.

---
+++ client/src/app/search-input/search-input.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { Response } from '../Response';
+import { Movie } from '../movie';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-search-input',
+  templateUrl: './search-input.component.html',
+  styleUrls: ['./search-input.component.css']
+})
+export class SearchInputComponent implements OnInit {
+
+  @Input() searchTerm!: string;
+  foundMovies!: Movie[];
+
+  constructor(private movieService: MovieService) { }
+
+  searchForMovie(): void {
+    this.movieService.searchForMovie(this.searchTerm).subscribe(response => this.foundMovies = response.results);
+  }
+
+  ngOnChanges(): void {
+    this.searchForMovie();
+  }
+
+  ngOnInit(): void {
+  }
+
+}
