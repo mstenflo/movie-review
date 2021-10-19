f58084fd81d97f322719100bcde747c8d7a10332
mstenflo <stenflo@gmail.com>
2021-10-19 07:55:04 -0700

    get user by username

--- server/movie_review/src/main/java/com/ex/movie_review/UserController.java
+++ server/movie_review/src/main/java/com/ex/movie_review/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "users")
@@ -30,23 +31,16 @@ public class UserController {
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public User getMessage() {
-        User user = new User();
-        user.setId(3L);
-        user.setEmail("es@srt.com");
-        user.setFirstname("John");
-        user.setLastname("Doe");
-        user.setUsername("johnny");
-        user.setPassword("123");
-        user.setPhone("123-123-1231");
-        return user;
+    public List<User> getAll() {
+        List<User> users = userRepository.findAll();
+        return users;
     }
-
-    @GetMapping(path="u")
-    public User GetUserWithId(@RequestParam(value = "id", required = false) long userId) {
-        System.out.println("Getting user with id " + userId);
-        return new User();
-    }
+//
+//    @GetMapping(path="u")
+//    public User GetUserWithId(@RequestParam(value = "id", required = false) long userId) {
+//        System.out.println("Getting user with id " + userId);
+//        return new User();
+//    }
 
     @GetMapping(path="{id}")
     @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRES_NEW, readOnly = true)
@@ -55,6 +49,14 @@ public class UserController {
         User user = userRepository.getById(id);
         System.out.println(user);
 
+        return user;
+    }
+
+    @GetMapping(path="username/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        System.out.println("Username: " + username);
+        User user = userRepository.findByUsername(username);
+        System.out.println(user);
         return user;
     }
 
