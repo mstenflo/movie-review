0866a11e6f0886cf74065303720819a77ea9b471
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-18 21:56:30 -0500

    Update movie.service.ts
    Updated: movie.service.ts.
    -> Implemented the searchForMovie(movie: string) method, which will attempt to search the IMDB database for a movie.
    -> Implemented functionality of the newly created LoggerService.

--- client/src/app/movie.service.ts
+++ client/src/app/movie.service.ts
@@ -1,26 +1,37 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import api from "../assets/hidden/apikey"
-import { Movie } from './movie';
 import { Response } from './Response';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
 
-  private moviesUrl = api.url;
+  private moviesUrl = api.top_movies_url;
+  private searchMoviesUrl = api.search_movies_url;
 
   constructor(
-    private http : HttpClient 
+    private http : HttpClient,
+    private logger : LoggerService
   ) { }
 
-  getMovies(): Observable<Response> {
+  getMovies(): Observable<any> {
     return this.http.get<Response>(this.moviesUrl).pipe(
       tap((_) => {
-        console.log("It got here?");
+        this.logger.log("Listing Top Movies...");
+      })
+    ); 
+  }
+
+  searchForMovie(movie: string): Observable<any> {  
+    console.log(this.searchMoviesUrl + movie)  
+    return this.http.get<Response>(this.searchMoviesUrl + movie).pipe(
+      tap((_) => {
+        this.logger.log("Searching movies for " + movie + "...");
       })
     ); 
   }
