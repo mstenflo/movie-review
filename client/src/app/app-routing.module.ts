df2590bbb165ba99fd4cbca030b83da94dfb951b
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-20 16:04:52 -0500

    Update app-routing.module.ts
    Updated: app-routing.module.ts.
    -> Implemented a route viewing a page detailing information about a specific movie.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { RegisterPageComponent } from './register-page/register-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { MoviePageComponent } from './movie-page/movie-page.component';
 
 const routes: Routes = [
   {path: "", component: MainPageComponent},
+  {path: "movies/:id", component: MoviePageComponent},
   {path: "login", component: LoginPageComponent},
   {path: "register", component: RegisterPageComponent}
 ];
