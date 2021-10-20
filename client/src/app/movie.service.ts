c5823c65e50d4de802b8ad3f0ebe622256302401
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-20 16:00:29 -0500

    Update movie.service.ts
    Updated: movie.service.ts.
    -> Removed references to the deleted Response interface.
    -> Implemented functionality for retrieving a movie's information based on its ID.

--- client/src/app/movie.service.ts
+++ client/src/app/movie.service.ts
@@ -1,23 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import api from "../assets/hidden/apikey"
-import { Response } from './Response';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
+
+import { Movie } from './movie';
 import { LoggerService } from './logger.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-
   private moviesUrl = api.top_movies_url;
   private searchMoviesUrl = api.search_movies_url;
+  private specificMovieUrl = api.specific_movie_url;
 
   constructor(
     private http : HttpClient,
     private logger : LoggerService
   ) { }
+
+  retrieveMovieById(id: string): Observable<Movie> {
+    return this.http.get<any>(this.specificMovieUrl + id).pipe(
+      tap((_) => {
+        this.logger.log("Retrieving movie #" + id);
+        this.logger.log(_);
+      })
+    ); 
+  }
 
   getMovies(): Observable<any> {
     return this.http.get<Response>(this.moviesUrl).pipe(
