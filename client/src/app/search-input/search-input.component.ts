258f7568be9f8b8628cce0154d9f1f402cf93b48
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-23 18:56:46 -0500

    Update SearchInput Component
    Updated: SearchInput Angular Component.
    -> Implemented animation functionality to the search list.
    -> Updated the styling of the search list.

--- client/src/app/search-input/search-input.component.ts
+++ client/src/app/search-input/search-input.component.ts
@@ -1,12 +1,32 @@
 import { Component, Input, OnInit, Output } from '@angular/core';
-import { EventEmitter } from '@angular/core';
 import { Movie } from '../movie';
 import { MovieService } from '../movie.service';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
 
 @Component({
   selector: 'app-search-input',
   templateUrl: './search-input.component.html',
-  styleUrls: ['./search-input.component.css']
+  styleUrls: ['./search-input.component.css'],
+  animations: [
+    trigger('searchOpenState', [
+      state("show", style({
+        display: "block",
+        opacity: 1
+      })),
+      state("hide", style({
+        display: "none",
+        opacity: 0,
+      })),
+      transition("show => hide", animate("200ms ease-out")),
+      transition("hide => show", animate("200ms ease-in"))
+    ])
+  ]
 })
 export class SearchInputComponent implements OnInit {
 
@@ -18,8 +38,14 @@ export class SearchInputComponent implements OnInit {
     this.userIsSearching = false;
   }
 
+  get menuState() {
+    return this.userIsSearching ? "show" : "hide";
+  }
+
   searchForMovie(): void {
     this.movieService.searchForMovie(this.searchTerm).subscribe(response => this.foundMovies = response.results);
+    
+    this.toggleSearch(true);
   }
 
   toggleSearch(flag: boolean): void {
@@ -30,9 +56,6 @@ export class SearchInputComponent implements OnInit {
     setTimeout(() => {
       this.toggleSearch(flag);
     }, 200);
-  }
-
-  ngOnChanges(): void {
   }
 
   ngOnInit(): void {
