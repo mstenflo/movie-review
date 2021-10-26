1bfbbb6385a70dd47a5f8bad3097bbd804b4321a
18vincent <18vincent@gmail.com>
2021-10-26 16:06:25 -0400

    fixes

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
+  @Input() username! : string;
+  @Input() password! : string;
 
-  constructor() { }
+  constructor(
+    private http : HttpClient
+  ) {
+  }
 
   ngOnInit(): void {
   }
 
+  handleClick() {
+    this.http.post("/api/users", {
+      username: this.username,
+      password: this.password
+    });
+    // e.preventDefault();
+    // const username = registerForm.username.value;
+    // const password = registerForm.password.value;
+    // let accountType;
+    // if (username === "employee" && password === "employees") {
+    //   accountType = true;
+    //   alert("You have successfully registered as an employee.");
+    //   location.reload();
+    // } else if (username === "manager" && password === "managers") {
+    //   accountType = false;
+    //   alert("You have successfully registered as a manager.");
+    //   location.reload();
+    // }
+    // else {
+    //   loginErrorMsg.style.opacity = 1;
+    // }
+  }
 }
