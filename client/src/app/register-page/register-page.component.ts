c02e107cffa898ca6939ecf473a9e568edd08e6f
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-26 16:01:48 -0500

    Implemented Base Register Page
    This component will act as the register page for the application. It still requires structure and css though.

--- client/src/app/register-page/register-page.component.ts
+++ client/src/app/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -7,7 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
 
-  constructor() { }
+  username!: string;
+  password!: string;
+  firstname!: string;
+  lastname!: string;
+  email! : string;
+  phone! : string;
+  user! : any;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router
+  ) { }
+
+  handleClick() : void {
+    console.log("YOU'RE HERE!");
+    this.http.post("http://localhost:8001/api/users", {
+      username: this.username,
+      password: this.password,
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email,
+      phone: this.phone
+    }).subscribe(data => this.user = data);
+
+    this.router.navigate(["/"]);
+  }
 
   ngOnInit(): void {
   }
