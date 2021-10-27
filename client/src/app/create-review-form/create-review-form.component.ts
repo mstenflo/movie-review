e25bbe9f7682e2bdf06835bb170f292609cedcb3
LeeHarperDev <42632613+LeeHarperDev@users.noreply.github.com>
2021-10-26 23:00:25 -0500

    Create CreateReviewForm Component
    Created: CreateReviewForm Angular Component.
    -> This component will be responsible for allowing the user to submit a review.

---
+++ client/src/app/create-review-form/create-review-form.component.ts
@@ -0,0 +1,54 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, Input, OnInit } from '@angular/core';
+import { LoggerService } from '../logger.service';
+
+@Component({
+  selector: 'app-create-review-form',
+  templateUrl: './create-review-form.component.html',
+  styleUrls: ['./create-review-form.component.css']
+})
+export class CreateReviewFormComponent implements OnInit {
+
+  username!: string;
+  subject!: string;
+  body!: string;
+
+  @Input()
+  movie_id!: string;
+
+  constructor(
+    private http : HttpClient,
+    private logger : LoggerService
+  ) { }
+
+  handleClick() : void {
+    let user : any;
+
+    this.http.get(`http://localhost:8001/api/users/username/${this.username}`)
+      .subscribe(data => {
+        user = data;
+
+        this.logger.log({
+          subject: this.subject,
+          body: this.body,
+          imdbid: this.movie_id,
+          reviewer: {
+            id: user.id
+          }
+        });
+
+        this.http.post(`http://localhost:8001/api/reviews`, {
+          subject: this.subject,
+          body: this.body,
+          imdbid: this.movie_id,
+          reviewer: {
+            id: user.id
+          }
+        }).subscribe(data => this.logger.log(data));
+      });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
